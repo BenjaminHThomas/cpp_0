f7b342a7c2d1216b86dc31e6d2752bba66ca82df
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-08-06 18:34:32 +0200

    first exercise completed

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,33 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 07:40:42 by bthomas           #+#    #+#             */
-/*   Updated: 2024/08/05 07:55:16 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/08/06 18:34:06 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string> // for std::string
+#include <iostream> // for std::cout
+#include <algorithm> // for std::transform
+#include <cctype> // for std::toupper
+
+void	print_msg(std::string msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *")
+{
+	std::transform(msg.begin(), msg.end(), msg.begin(), ::toupper);
+	std::cout << msg;
+}
+
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	std::string	input_str;
+	int			i = 1;
+
+	if (ac == 1)
+		print_msg();
+	else
+	{
+		while (i < ac)
+			print_msg(av[i++]);
+	}
+	std::cout << "\n";
 	return (0);
 }
