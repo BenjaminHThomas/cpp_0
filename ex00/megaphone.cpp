0305cd85dd92dee598dc8fe42d55eff03f646f74
Ben Thomas <benjamin.h.thomas1@gmail.com>
2024-08-06 18:39:23 +0200

    replaced while loop with for loop

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 07:40:42 by bthomas           #+#    #+#             */
-/*   Updated: 2024/08/06 18:35:03 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/08/06 18:38:27 by bthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,12 @@ void	print_msg(std::string msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *")
 
 int	main(int ac, char **av)
 {
-	int			i = 1;
-
 	if (ac == 1)
 		print_msg();
 	else
 	{
-		while (i < ac)
-			print_msg(av[i++]);
+		for (int i = 1; i < ac; ++i)
+			print_msg(av[i]);
 	}
 	std::cout << "\n";
 	return (0);
